ce3b38a2f532dc326e290ba147cdde260730ca20
watano1168 <watano1168@gmail.com>
2021-06-02 18:14:01 +0900

    ページ内にリンクを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function HomePage(): JSX.Element {
   return (
     <div>
       <h2>Home page</h2>
+      <Link href="/portal">
+        <a>Go to portal</a>
+      </Link>
     </div>
   );
 }
--- src/pages/portal.tsx
+++ src/pages/portal.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function PortalPage(): JSX.Element {
   return (
     <div>
       <h2>Portal page</h2>
+      <Link href="/">
+        <a>Go to home</a>
+      </Link>
+      <Link href="/posts">
+        <a>Go to posts</a>
+      </Link>
+      <Link href="/works">
+        <a>Go to works</a>
+      </Link>
     </div>
   );
 }
