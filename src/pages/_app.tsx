b5b1a502a86aea893b791060b6369661ed68dbd3
watano1168 <watano1168@gmail.com>
2021-06-02 18:30:06 +0900

    global cssのパスを変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import "../styles/globals.css";
+import "../globals.css";
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
