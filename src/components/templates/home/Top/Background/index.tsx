1f6799e8b3ad7efcf9c3a2e0039950d7e77acd5e
watano1168 <watano1168@gmail.com>
2021-07-07 10:55:51 +0900

    トップのアニメーションを追加

--- src/components/templates/home/Top/Background/index.tsx
+++ src/components/templates/home/Top/Background/index.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import styles from "./index.module.scss";
-import { getComponentTemplate } from "src/lib/component";
+import { assignClasses, getComponentTemplate } from "src/lib/component";
 import FixedParallax from "src/components/accessories/FixedParallax";
 
-const Background = getComponentTemplate(() => (
-  <FixedParallax className={styles.parallax} startInnerOrigin={0} endInnerOrigin={-0.5}>
-    <img className={styles.image} src="/top.jpg" alt="トップ画像" />
-  </FixedParallax>
-));
+const Background = getComponentTemplate(() => {
+  const [display, setDisplay] = useState<boolean>(false);
+  const onDisplay = () => setDisplay(true);
+
+  return (
+    <FixedParallax className={styles.parallax} startInnerOrigin={0} endInnerOrigin={-0.5}>
+      <div className={styles.inner} ref={onDisplay}>
+        <img
+          className={assignClasses(styles.image, display ? styles.active : styles.inactive)}
+          src="/top.jpg"
+          alt="トップ画像"
+        />
+        <div className={assignClasses(styles.effect, display ? styles.active : styles.inactive)} />
+      </div>
+    </FixedParallax>
+  );
+});
 
 export default Background;
