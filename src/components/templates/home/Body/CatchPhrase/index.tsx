4e158afb1abe2ccfebde24da1cb9a227eb3241c3
watano1168 <watano1168@gmail.com>
2021-07-07 13:50:31 +0900

    トップのアニメーションを修正

--- src/components/templates/home/Body/CatchPhrase/index.tsx
+++ src/components/templates/home/Body/CatchPhrase/index.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import VisibilitySensor from "react-visibility-sensor";
 import styles from "./index.module.scss";
-import { getComponentTemplate } from "src/lib/component";
+import { assignClasses, getComponentTemplate } from "src/lib/component";
 
-const CatchPhrase = getComponentTemplate(() => (
-  <div className={styles.container}>
-    <div className={styles.inner}>
-    </div>
-  </div>
-));
+const CatchPhrase = getComponentTemplate(() => {
+  const [display, setDisplay] = useState(false);
+  const onDisplay = (isVisible: boolean) => setDisplay(isVisible || display);
+
+  return (
+    <VisibilitySensor onChange={onDisplay}>
+      <div className={styles.container}>
+        <div className={assignClasses(styles.phrase, styles.first)}>
+          <h1 className={assignClasses(styles.text, display ? styles.active : styles.inactive)}>
+            この<span className={styles.strong}>筏</span>は私を、
+          </h1>
+        </div>
+        <div className={assignClasses(styles.phrase, styles.second)}>
+          <h1 className={assignClasses(styles.text, display ? styles.active : styles.inactive)}>
+            どこへ連れて行くんだろう。
+          </h1>
+        </div>
+      </div>
+    </VisibilitySensor>
+  );
+});
 
 export default CatchPhrase;
