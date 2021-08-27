2943e3d0bb1b10df6d16d3740d4425138d03375f
watano1168 <watano1168@gmail.com>
2021-08-28 06:02:57 +0900

    スマートフォン用のメニューボタンのレイアウトを追加

--- src/lib/fooks/element-position.ts
+++ src/lib/fooks/element-position.ts
@@ -9,6 +9,7 @@ export default function useElementPosition(
   const [y, setY] = useState(initialY);
 
   useEffect(() => {
+    // It's prefer to subscribe an event publishing a position value than reference the value, but I couldn't find how to.
     if (!element?.current) return;
     setX(element.current.scrollLeft);
     setY(element.current.scrollTop);
