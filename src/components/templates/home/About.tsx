f11e00a533de88e95e773b3454673b8100c379a7
watano1168 <watano1168@gmail.com>
2021-08-29 22:39:36 +0900

    コンテンツメニューのモバイル用表示を追加

--- src/components/templates/home/About.tsx
+++ src/components/templates/home/About.tsx
@@ -1,22 +1,52 @@
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import VisibilitySensor from "react-visibility-sensor";
 import styles from "./About.module.scss";
 import { assignClasses } from "src/lib/helper";
+import useElementSize from "src/lib/fooks/element-size";
 
 const About = (): JSX.Element => {
   const [display, setDisplay] = useState(false);
+  const containerRef = useRef(null);
+  const frontRef = useRef(null);
+  const [containerWidth] = useElementSize(containerRef);
+  const [, frontHeight] = useElementSize(frontRef);
+  const imageAspectRatio = 2104 / 1321;
+
+  const getContainerHeight = useCallback(
+    () => Math.max(containerWidth / imageAspectRatio, frontHeight),
+    [containerWidth, frontHeight]
+  );
+
+  const getBackgroundStyle = useCallback((): React.CSSProperties => {
+    const containerHeight = getContainerHeight();
+    const containerAspectRatio = containerWidth / containerHeight;
+    if (containerAspectRatio >= imageAspectRatio) {
+      return {
+        width: containerWidth,
+        height: containerWidth / imageAspectRatio
+      };
+    } else {
+      return {
+        width: containerHeight * imageAspectRatio,
+        height: containerHeight
+      };
+    }
+  }, [containerWidth, getContainerHeight()]);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.background}>
+    <div className={styles.container} ref={containerRef} style={{ height: getContainerHeight() }}>
+      <div className={styles.background} style={getBackgroundStyle()}>
         <img className={styles.image} src="/img/note_clipped.png" alt="" />
       </div>
 
-      <div className={styles.content}>
+      <div className={styles.front} ref={frontRef}>
         <div className={styles.title}>
           <VisibilitySensor onChange={(isVisible) => setDisplay(display || isVisible)}>
             <span className={assignClasses(styles.text, display ? styles.active : styles.inactive)}>
-              この<span className={styles.strong}>筏</span>は私を、どこへ連れてゆくんだろう。
+              <span className={styles.first}>
+                この<span className={styles.strong}>筏</span>は私を、
+              </span>
+              <span className={styles.last}>どこへ連れてゆくんだろう。</span>
             </span>
           </VisibilitySensor>
         </div>
@@ -30,9 +60,7 @@ const About = (): JSX.Element => {
           <div className={styles.section}>
             <p>そのときに、作りたいと思ったものを作って楽しむ。</p>
             <p>現在は主にゲームやサービス等を制作しているが、活動範囲は限定していない。</p>
-            <p>
-              制作の途中経過やアイデアなどは当サイトの「TALKS」へ投稿し、制作物やプロジェクトは「PROJECTS」へ投稿する。
-            </p>
+            <p>制作の途中経過やアイデアなどは当サイトのTALKSへ投稿し、制作物やプロジェクトはPROJECTSへ投稿する。</p>
           </div>
         </div>
       </div>
