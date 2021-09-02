83b7b374736b0e374b0209869b83e3378b71ddb4
watano1168 <watano1168@gmail.com>
2021-09-02 23:24:37 +0900

    Webフォントの設定をカスタムドキュメントに移動

--- src/components/templates/layout/Layout.tsx
+++ src/components/templates/layout/Layout.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useRef, useState } from "react";
-import Head from "next/head";
 import styles from "./Layout.module.scss";
 import SideMenuButton from "components/templates/layout/SideMenuButton";
 import TopButton from "components/templates/layout/TopButton";
@@ -21,24 +20,6 @@ const Layout = ({ children }: Props): JSX.Element => {
 
   return (
     <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(d) {
-                var config = {
-                  kitId: 'kvy7hzy',
-                  scriptTimeout: 3000,
-                  async: true
-                },
-                h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-              })(document);
-            `
-          }}
-        />
-        <title />
-      </Head>
-
       <div ref={topRef} />
 
       {children}
