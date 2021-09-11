16242338a0497ebdf665d8bf272ac53ec9c50b6c
watano1168 <watano1168@gmail.com>
2021-09-11 21:25:29 +0900

    FixedImageがなぜか縦に2pxずれてしまう不具合を修正

--- src/components/functions/image/FixedImage.tsx
+++ src/components/functions/image/FixedImage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from "react";
+import { CSSProperties, useCallback, useRef } from "react";
 import Image, { ImageProps } from "next/image";
 import styles from "./FixedImage.module.scss";
 import useElementSize from "lib/fooks/element-size";
@@ -13,19 +13,14 @@ const FixedImage = ({ widthRate = 16, heightRate = 9, lock = "width", ...props }
   const containerRef = useRef<HTMLDivElement>(null);
   const [containerWidth, containerHeight] = useElementSize(containerRef);
 
-  const getWidth = useCallback(() => {
-    if (lock === "width") return containerWidth;
-    return (containerHeight * widthRate) / heightRate;
-  }, [lock, containerWidth, containerHeight, widthRate, heightRate]);
-
-  const getHeight = useCallback(() => {
-    if (lock === "height") return containerHeight;
-    return (containerWidth * heightRate) / widthRate;
+  const getContainerStyle = useCallback((): CSSProperties => {
+    if (lock === "width") return { width: "100%", height: (containerWidth * heightRate) / widthRate };
+    else return { width: (containerHeight * widthRate) / heightRate, height: "100%" };
   }, [lock, containerWidth, containerHeight, widthRate, heightRate]);
 
   return (
-    <div className={styles.container} ref={containerRef}>
-      <Image className={styles.image} width={getWidth()} height={getHeight()} objectFit="contain" {...props} />
+    <div className={styles.container} ref={containerRef} style={getContainerStyle()}>
+      <Image layout="fill" {...props} />
     </div>
   );
 };
