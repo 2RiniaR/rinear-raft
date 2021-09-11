b84cabc0587e17324e52e21ebfc5f8d43cf0dc46
watano1168 <watano1168@gmail.com>
2021-09-11 14:02:20 +0900

    レイアウトのレスポンシブ対応を追加

--- src/components/templates/talks-index/TalksViewer.tsx
+++ src/components/templates/talks-index/TalksViewer.tsx
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import Link from "next/link";
 import styles from "./TalksViewer.module.scss";
 import TalkHeading from "./TalkHeading";
 import { getContentPath, TalkContentHead } from "lib/contents";
+import useElementSize from "lib/fooks/element-size";
+import useViewSize from "lib/fooks/view-size";
+import { getMediaQueryDeviceType } from "lib/layout";
 
 type Props = {
   heads: TalkContentHead[];
 };
 
-const TalksViewer = ({ heads }: Props): JSX.Element => (
-  <div className={styles.container}>
-    {heads.map((head) => (
-      <div className={styles.element} key={head.id}>
-        <Link href={getContentPath(head)}>
-          <a className={styles.link}>
-            <TalkHeading head={head} />
-          </a>
-        </Link>
-      </div>
-    ))}
-  </div>
-);
+const TalksViewer = ({ heads }: Props): JSX.Element => {
+  const itemMinWidth = 280;
+  const [viewWidth] = useViewSize();
+  const containerRef = useRef(null);
+  const [containerWidth] = useElementSize(containerRef);
+  const getItemsPerRow = useCallback(() => {
+    if (getMediaQueryDeviceType(viewWidth) == "phone") return 1;
+    return Math.max(Math.floor(containerWidth / itemMinWidth), 1);
+  }, [containerWidth, itemMinWidth, viewWidth]);
+  const getItemWidth = useCallback(() => containerWidth / getItemsPerRow(), [getItemsPerRow(), containerWidth]);
+
+  return (
+    <div className={styles.container} ref={containerRef}>
+      {heads.map((head) => (
+        <div className={styles.element} key={head.id} style={{ width: getItemWidth() }}>
+          <Link href={getContentPath(head)}>
+            <a className={styles.link}>
+              <TalkHeading head={head} />
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default TalksViewer;
