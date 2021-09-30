d3f9979c88444d43a3d03d4c2b01b5650d267ed9
watano1168 <watano1168@gmail.com>
2021-10-01 05:05:48 +0900

    サムネイル画像の設定

--- src/data/contents/projects/rinear-official-site/index.tsx
+++ src/data/contents/projects/rinear-official-site/index.tsx
@@ -11,7 +11,12 @@ const rinearOfficialSite: ProjectContent = {
   genre: "projects",
   id: "rinear-official-site",
   title: "RineaR公式サイト",
-  thumbnailsPath: ["/img/test.png", "/img/test.png", "/img/test.png", "/img/test.png"],
+  thumbnailsPath: [
+    "/contents/projects/rinear-official-site/HomePage1.jpg",
+    "/contents/projects/rinear-official-site/HomePage2.jpg",
+    "/contents/projects/rinear-official-site/HomePage3.jpg",
+    "/contents/projects/rinear-official-site/HomePage4.jpg"
+  ],
   updatedAt: dayjs("2021-07-24"),
   releasedAt: dayjs("2021-04-30"),
   description: "RineaRのプロジェクトや投稿を公開するホームページ。Riniaが活動拠点とする筏。",
