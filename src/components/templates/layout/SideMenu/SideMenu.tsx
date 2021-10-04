9461fd68769c1aca598c78ea16986ecd5f3e3360
watano1168 <watano1168@gmail.com>
2021-10-04 18:00:24 +0900

    サイドメニューのDOMを簡略化

---
+++ src/components/templates/layout/SideMenu/SideMenu.tsx
@@ -0,0 +1,34 @@
+import styles from "./SideMenu.module.scss";
+import SideMenuItem, { SideMenuItemProps } from "./SideMenuItem";
+import SideMenuOpener from "./SideMenuOpener";
+import StaticImage from "components/functions/image/StaticImage";
+import leftArrowPic from "public/img/LeftArrow.png";
+import logoPic from "public/img/logo.png";
+import talksPic from "public/img/talks.png";
+import projectsPic from "public/img/projects.png";
+
+type Props = {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+};
+
+const items: SideMenuItemProps[] = [
+  { href: "/", markSrc: logoPic, name: "HOME" },
+  { href: "/talks", markSrc: talksPic, name: "TALKS" },
+  { href: "/projects", markSrc: projectsPic, name: "PROJECTS" }
+];
+
+const SideMenu = ({ open, setOpen }: Props): JSX.Element => (
+  <SideMenuOpener open={open} setOpen={setOpen}>
+    <button className={styles.backButton} onClick={() => setOpen(false)} name="戻る" aria-label="戻る">
+      <StaticImage src={leftArrowPic} alt="戻る" />
+    </button>
+    <div className={styles.index}>
+      {items.map((item) => (
+        <SideMenuItem {...item} key={item.name} />
+      ))}
+    </div>
+  </SideMenuOpener>
+);
+
+export default SideMenu;
