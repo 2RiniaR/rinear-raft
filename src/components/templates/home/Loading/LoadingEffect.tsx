b7956f2b17838d30a5114c3fe0328dd878341a8d
watano1168 <watano1168@gmail.com>
2021-10-11 18:18:01 +0900

    ロード画面のトランジションの追加

--- src/components/templates/home/Loading/LoadingEffect.tsx
+++ src/components/templates/home/Loading/LoadingEffect.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import styles from "./LoadingEffect.module.scss";
+import { assignClasses } from "lib/helper";
 
 type Props = {
   loading: boolean;
 };
 
 const LoadingEffect = ({ loading }: Props): JSX.Element => (
-  <div className={styles.screen} style={{ display: loading ? "block" : "none" }}>
-    Loading
-  </div>
+  <div className={assignClasses(styles.screen, loading ? styles.loading : styles.completed)}>Loading</div>
 );
 
 export default LoadingEffect;
