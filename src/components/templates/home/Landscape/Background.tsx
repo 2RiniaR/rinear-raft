08a5447a6e72fa07bde6541aedab6e4e6aa524d3
watano1168 <watano1168@gmail.com>
2022-06-22 15:00:30 +0900

    Landscapeを整理

--- src/components/templates/home/Landscape/Background.tsx
+++ src/components/templates/home/Landscape/Background.tsx
@@ -6,15 +6,33 @@ import styles from "./Background.module.scss";
 import { assignClasses } from "lib/helper";
 import landscapePic from "public/img/background.webp";
 import LazyStaticImage from "components/functions/lazy/LazyStaticImage";
+import holeEffectPic from "public/img/hole_effect.webp";
+import windEffectPic from "public/img/wind_effect.png";
 
 const Background = (): JSX.Element => (
-  <LazyStaticImage
-    className={assignClasses(styles.image)}
-    src={landscapePic}
-    alt="背景"
-    layout="responsive"
-    quality={100}
-  />
+  <div className={styles.container}>
+    <LazyStaticImage
+      className={assignClasses(styles.picture)}
+      src={landscapePic}
+      alt="背景"
+      layout="responsive"
+      quality={100}
+    />
+    <LazyStaticImage className={styles.holeEffect} src={holeEffectPic} alt="背景" layout="responsive" quality={100} />
+    <LazyStaticImage
+      className={assignClasses(styles.windEffect, styles.layer1)}
+      src={windEffectPic}
+      alt="風のエフェクト"
+      layout="responsive"
+    />
+    <LazyStaticImage
+      className={assignClasses(styles.windEffect, styles.layer2)}
+      src={windEffectPic}
+      alt="風のエフェクト"
+      layout="responsive"
+    />
+    <div className={styles.smogEffect} />
+  </div>
 );
 
 export default Background;
--- src/components/templates/home/Landscape/Landscape.tsx
+++ src/components/templates/home/Landscape/Landscape.tsx
@@ -2,16 +2,13 @@
  * Copyright (c) 2022 RineaR. All rights reserved.
  */
 
-import { assignClasses } from "../../../../lib/helper";
 import styles from "./Landscape.module.scss";
 import Background from "./Background";
-import HoleEffect from "./HoleEffect";
 import Shadow from "./Shadow";
-import WindEffect from "./WindEffect";
 import Logo from "./Logo";
-import SmogEffect from "./SmogEffect";
 import Rift from "./Rift";
 import Phantom from "./Phantom";
+import { assignClasses } from "lib/helper";
 import FixedParallax from "components/functions/parallax/Parallax";
 import letterIcon from "public/img/letters.png";
 import scenarioIcon from "public/img/scenarios.png";
@@ -20,45 +17,26 @@ type Props = {
   menuActive: boolean;
 };
 
-const Layer2 = ({ menuActive }: Props): JSX.Element => (
-  <div className={styles.layer}>
-    <Background />
-    <HoleEffect />
-    <WindEffect />
-    <SmogEffect />
-    <Rift active={menuActive} />
-  </div>
-);
-
-const Layer1 = ({ menuActive }: Props): JSX.Element => (
-  <div className={styles.layer}>
-    <Logo active={menuActive} />
-    <Shadow />
-  </div>
-);
-
-const Layer0 = ({ menuActive }: Props): JSX.Element => (
-  <div className={styles.layer}>
-    <div className={assignClasses(styles.letters, !menuActive ? styles.hidden : "")}>
-      <Phantom title="LETTERS" subtitle="制作状況" icon={letterIcon} href="/letters" />
-    </div>
-    <div className={assignClasses(styles.scenarios, !menuActive ? styles.hidden : "")}>
-      <Phantom title="SCENARIOS" subtitle="企画／作品" icon={scenarioIcon} href="/scenarios" />
-    </div>
-  </div>
-);
-
-const Landscape = (props: Props): JSX.Element => (
+const Landscape = ({ menuActive }: Props): JSX.Element => (
   <header className={styles.view}>
     <div className={styles.elements}>
       <FixedParallax startInnerOrigin={0} endInnerOrigin={-0.1}>
-        <Layer2 {...props} />
+        <Background />
       </FixedParallax>
       <FixedParallax startInnerOrigin={0} endInnerOrigin={-0.2}>
-        <Layer1 {...props} />
+        <Shadow />
+        <Logo active={menuActive} />
+      </FixedParallax>
+      <FixedParallax startInnerOrigin={0} endInnerOrigin={-0.1}>
+        <Rift active={menuActive} />
       </FixedParallax>
       <FixedParallax startInnerOrigin={0} endInnerOrigin={-0.3}>
-        <Layer0 {...props} />
+        <div className={assignClasses(styles.letters, !menuActive ? styles.hidden : "")}>
+          <Phantom title="LETTERS" subtitle="制作状況" icon={letterIcon} href="/letters" />
+        </div>
+        <div className={assignClasses(styles.scenarios, !menuActive ? styles.hidden : "")}>
+          <Phantom title="SCENARIOS" subtitle="企画／作品" icon={scenarioIcon} href="/scenarios" />
+        </div>
       </FixedParallax>
     </div>
   </header>
