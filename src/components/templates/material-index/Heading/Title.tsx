c6b2c29d6539298210b423e6e2d526a085385af1
watano1168 <watano1168@gmail.com>
2022-07-03 21:28:36 +0900

    サーバーサイドとクライアントサイドでのレンダリング結果が異なるエラーを抑制

--- src/components/templates/material-index/Heading/Title.tsx
+++ src/components/templates/material-index/Heading/Title.tsx
@@ -15,7 +15,9 @@ const Title = ({ head }: Props): JSX.Element => (
   <div className={styles.layout}>
     <h2 className={styles.title}>{head.title}</h2>
     <p className={styles.description}>{head.description}</p>
-    <p className={styles.updatedAt}>{"最終更新：" + formatExceededTime(dayjs(), head.updatedAt)}</p>
+    <p className={styles.updatedAt} suppressHydrationWarning={true}>
+      {"最終更新：" + formatExceededTime(dayjs(), head.updatedAt)}
+    </p>
     <p className={styles.releasedAt}>
       <span className={styles.text}>{formatDisplayDate(head.releasedAt)}</span>
     </p>
