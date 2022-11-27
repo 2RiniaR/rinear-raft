231c318444b5172f2dae8081db4f4c6fe50abe05
watano1168 <watano1168@gmail.com>
2022-11-27 21:46:32 +0900

    コンテンツの更新

--- src/materials.ts
+++ src/materials.ts
@@ -8,7 +8,7 @@ export const materials: { [id: string]: Material } = {
     type: "important",
     dirname: "M001_maze-escape",
     image: require("public/contents/materials/M001_maze-escape/stage1.webp"),
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
     releasedAt: dayjs("2016-10-18").toDate(),
     description: "上下左右にプレイヤーを移動し、制限時間以内にゴールを目指す2Dアクション脱出ゲーム。"
   },
@@ -16,7 +16,7 @@ export const materials: { [id: string]: Material } = {
     title: "2. Busters Mission",
     dirname: "M002_busters-mission",
     image: require("public/contents/materials/M002_busters-mission/stage3-2.webp"),
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
     releasedAt: dayjs("2018-09-05").toDate(),
     description: "敵の攻撃を避けながら、ステージ内にいるすべての敵を倒していく2Dアクションバトルゲーム。"
   },
@@ -24,7 +24,7 @@ export const materials: { [id: string]: Material } = {
     title: "3. ポップコーン職人",
     dirname: "M003_popcorn-chef",
     image: require("public/contents/materials/M003_popcorn-chef/game1.webp"),
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
     releasedAt: dayjs("2020-04-05").toDate(),
     description: "2人のプレイヤーがリアルタイムでポップコーンを調理し、食べた量で競うカジュアルゲーム。"
   },
@@ -32,7 +32,7 @@ export const materials: { [id: string]: Material } = {
     title: "4. メイジ・シミュレータ",
     dirname: "M004_mage-simulator",
     image: require("public/contents/materials/M004_mage-simulator/title.webp"),
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
     releasedAt: dayjs("2021-02-25").toDate(),
     description: "モーションセンサとマイクを使って、魔法を詠唱し発動できるアプリケーション。"
   },
@@ -40,7 +40,7 @@ export const materials: { [id: string]: Material } = {
     title: "5. エライさんbot",
     dirname: "M005_marvelous",
     image: require("public/contents/materials/M005_marvelous/page1.webp"),
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
     releasedAt: dayjs("2021-04-30").toDate(),
     description: "みんなの「えらい！」をカウントしてくれるdiscord Bot。"
   },
@@ -48,7 +48,7 @@ export const materials: { [id: string]: Material } = {
     title: "6. キミフィール",
     dirname: "M006_kimifeel",
     image: require("public/contents/materials/M006_kimifeel/page1.webp"),
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
     releasedAt: dayjs("2022-02-16").toDate(),
     description: "周りの人々に自分のプロフィールを書いてもらえるサービス。"
   },
@@ -56,7 +56,7 @@ export const materials: { [id: string]: Material } = {
     title: "7. み　て　る　よ",
     dirname: "M007_eyes",
     image: require("public/contents/materials/M007_eyes/eyes.webp"),
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
     releasedAt: dayjs("2022-02-25").toDate(),
     description: "チャットでの『入力中...』が長時間続いていると、『👁👁』を送ってくるdiscord bot。"
   },
@@ -64,7 +64,7 @@ export const materials: { [id: string]: Material } = {
     title: "8. RineaRの筏 chap.1",
     dirname: "M008_rinear-raft-chap1",
     image: require("public/contents/materials/M008_rinear-raft-chap1/top.webp"),
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
     releasedAt: dayjs("2022-07-03").toDate(),
     description: "RineaRに関することを公開するホームページ。"
   },
@@ -72,7 +72,7 @@ export const materials: { [id: string]: Material } = {
     title: "9. グラスモーズ",
     dirname: "M009_glass-moze",
     image: require("public/contents/materials/M009_glass-moze/play1.jpg"),
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
     releasedAt: dayjs("2022-09-13").toDate(),
     description:
       "いろんな「グラス」で敵の弾を吸引してエネルギーを溜め、攻撃や強化をしてモンスターを殲滅する2Dアクションバトルゲーム。"
@@ -81,8 +81,8 @@ export const materials: { [id: string]: Material } = {
     title: "10. RineaRの筏 chap.2",
     dirname: "M010_rinear-raft-chap2",
     image: require("public/contents/materials/M010_rinear-raft-chap2/concept2.png"),
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
-    releasedAt: dayjs("2022-11-14").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
+    releasedAt: dayjs("2022-11-27").toDate(),
     description:
       "RineaRに関することを公開するホームページ。以前と比べて解釈が進み、RineaRの意味が変更された。また、デザインも大きく変更されており、全体的に明るくなっている。"
   },
@@ -90,7 +90,7 @@ export const materials: { [id: string]: Material } = {
     title: "11. Beat a bit",
     dirname: "M011_beat-a-bit",
     image: require("public/contents/materials/M011_beat-a-bit/wip1.jpg"),
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
     releasedAt: undefined,
     description:
       "【進行中】「何秒後にどの能力を使いたいか」を決めた後、キャラクターを操作してゴールを目指す『謎解き』×『リズム』×『アクション』ゲーム。"
@@ -100,7 +100,7 @@ export const materials: { [id: string]: Material } = {
     type: "important",
     dirname: "M012_made-highlow",
     image: lockedPic,
-    updatedAt: dayjs("2022-11-13 21:00:00").toDate(),
+    updatedAt: dayjs("2022-11-27 21:00:00").toDate(),
     releasedAt: undefined,
     description:
       "【進行中】キャラクターを「大切に育てて立派になるのを目指す」か、「切り札とする代わりに犠牲にする」かの選択を迫られる、対戦型シミュレーションRPG。"
