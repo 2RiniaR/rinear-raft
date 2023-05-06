a08e220b05b03db8fbb67ed284d8557fe18d809e
watano1168 <watano1168@gmail.com>
2023-05-07 01:45:45 +0900

    lettersのコンテンツページをmicroCMSに対応

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 import { StaticImageData } from "next/image";
 
-export type Letter = {
-  image?: StaticImageData;
-  type?: "important";
-  dirname?: string;
-  title: string;
-  description: string;
-  updatedAt: Date;
-  releasedAt: Date | undefined;
-};
-
 export type Material = {
   image?: StaticImageData;
   type?: "important";
--- src/libs/microcms/helpers.ts
+++ src/libs/microcms/helpers.ts
@@ -1,6 +1,6 @@
 import { MicroCMSImage } from "microcms-js-sdk";
 import { StaticImageData } from "next/image";
-import { formatExceededTime } from "../../functions";
+import { formatDisplayDate, formatExceededTime } from "../../functions";
 
 export function convertToNextImageProps(
   image: MicroCMSImage | undefined,
@@ -24,4 +24,8 @@ export function convertToNextImageProps(
 export function formatExceededTimeFromString(from: string | undefined): string {
   if (from === undefined) return "---";
   return formatExceededTime(new Date(), new Date(from));
+}
+
+export function formatDisplayDateFromString(from: string | undefined): string {
+  return formatDisplayDate(from !== undefined ? new Date(from) : undefined);
 }
